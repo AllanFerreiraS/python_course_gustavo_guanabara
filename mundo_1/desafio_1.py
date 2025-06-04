d57af7fe5e97b5d9c6f0037ed00431d265a1e0c5
AllanFerreiraS <afsn01@protonmail.com>
2025-06-03 21:14:20 -0300

    Add folder 'mundo_1'

---
+++ mundo_1/desafio_1.py
@@ -0,0 +1,10 @@
+"""
+Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+de acordo com o valor digitado.
+"""
+print("="*10, end=" ")
+print("DESAFIO 01", end=" ")
+print("="*10)
+
+nome = input("Qual é o seu nome? ")
+print(f"Olá \'{nome}\'! Prazer em te conhecer!");
