c610dba622e653f738cf63cfc44d6f87300f966b
AllanFerreiraS <afsn01@protonmail.com>
2025-06-05 21:53:33 -0300

    Add desafio 2 e 3

---
+++ mundo_1/desafio_2.py
@@ -0,0 +1,18 @@
+"""
+Crie um script
+Python que leia o
+dia, o mês e o ano de
+nascimento de uma
+pessoa e mostre
+uma mensagem com
+a data formatada.
+"""
+print("="*10, end="")
+print(" DESAFIO 02 ", end="")
+print("="*10)
+
+day = input("Dia: ")
+month = input("Mês: ")
+year = input("Ano: ")
+
+print(f"Data atual: \'{day}\' de \'{month}\' de \'{year}\'.")
---
+++ mundo_1/desafio_3.py
@@ -0,0 +1,15 @@
+"""
+Crie um script
+Python que leia dois
+números e tente
+mostrar a soma
+entre eles.
+"""
+
+print("="*10, end="")
+print(" DESAFIO 03 ", end="")
+print("="*10)
+
+n1 = float(input("Primeiro número: "))
+n2 = float(input("Segundo número: "))
+print(f"{n1} + {n2} = {n1 + n2}")
